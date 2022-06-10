e89c9a090a49ec026e4deae913a19bc48e8b49b1
vexe141 <98473098+vexe141@users.noreply.github.com>
2022-06-10 20:18:58 +0545

    improved readability

--- some.cpp
+++ some.cpp
@@ -3,72 +3,68 @@
 #include <string>
 
 int n;
-using namespace std;
-int main();
-void calculate(int m){
-n+=m;
-cout<<"Are you done?"<<"\n";
-bool a;
-cin>>a;
-if(a==true){
-cout<<"The total price is :"<<n;
-}else if(a==false){
-    main();
-}
 
+int main();
+
+void calculate(int m) {
+    n += m;
+    std::cout << "Are you done?" << "\n";
+    bool a;
+    std::cin >> a;
+    
+    if(a == true) {
+        std::cout<<"The total price is :" << n;
+    } else if(a == false) {
+        main();
+    }
 }
 
 void snacks(){
-int m,n;
-cout<<"1.Lolipop                   Rs200"<<"\n";
-cout<<"2.Chicken chilly            Rs300"<<"\n\n\n";
-cout<<"Enter your choice :";
-cin>>m;
-if(m==1){
-    n=200;
-    calculate(n);
-}
-else if(m==2){
-    n=300;
-     calculate(n);
-}
-
-}
-
-void soup(){
-    int m,n;
-cout<<"1.Human soup                   Rs150"<<"\n";
-cout<<"2.Dost soup                    Rs210"<<"\n";
-
-cout<<"Enter your choice :";
-cin>>m;
-
-if(m==1){
-    n=150;
-    calculate(n);
-}
-else if(m==2){
-    n=210;
-     calculate(n);
-}
+    int m, n;
+    std::cout << "1.Lolipop\tRs200" << "\n";
+    std::cout<<"2.Chicken chilly\tRs300" << "\n\n\n";
+    std::cout<<"Enter your choice :";
+    cin >> m;
+    if(m == 1) {
+        n = 200;
+        calculate(n);
+    }
+    else if(m == 2){
+        n = 300;
+        calculate(n);
+    }
 }
 
-int main(){
+void soup() {
+    int m, n;
+    std::cout << "1.Human soup\tRs150" << "\n";
+    std::cout << "2.Dost soup\tRs210" << "\n"; // the hell is dost soup?
+
+    std::cout << "Enter your choice :";
+    std::cin >> m;
+
+    if(m == 1) {
+        n = 150;
+        calculate(n);
+    } else if(m == 2) {
+        n = 210;
+        calculate(n);
+    }
+}
+
+int main() {
     int j;
     
-  cout<<"--------MENU---------"<<endl;
-  cout<<"1.Snacks"<<"\n"; 
-  cout<<"2.Soup"<<"\n";
+    std::cout << "--------MENU---------" << "\n";
+    std::cout << "1.Snacks" << "\n"; 
+    std::cout << "2.Soup" << "\n";
 
-  cout<<"Select :";
- cin>>j;
+    std::cout << "Select :";
+    std::cin >> j;
 
- if(j==1){
-    snacks();
- } 
- else if(j==2){
-  soup();
- }
-
-   
+    if(j == 1) {
+        snacks();
+    } else if(j == 2) {
+        soup();
+    }   
 }
