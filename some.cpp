6dd1d69d6b9148d745baeb88cb7effe9857e993a
dost009 <queenkingpotato@gmail.com>
2022-06-10 19:57:46 +0545

    dost009

--- some.cpp
+++ some.cpp
@@ -1,18 +1,74 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <string>
+
+int n;
+using namespace std;
+int main();
+void calculate(int m){
+n+=m;
+cout<<"Are you done?"<<"\n";
+bool a;
+cin>>a;
+if(a==true){
+cout<<"The total price is :"<<n;
+}else if(a==false){
+    main();
+}
+
+}
+
+void snacks(){
+int m,n;
+cout<<"1.Lolipop                   Rs200"<<"\n";
+cout<<"2.Chicken chilly            Rs300"<<"\n\n\n";
+cout<<"Enter your choice :";
+cin>>m;
+if(m==1){
+    n=200;
+    calculate(n);
+}
+else if(m==2){
+    n=300;
+     calculate(n);
+}
+
+}
+
+void soup(){
+    int m,n;
+cout<<"1.Human soup                   Rs150"<<"\n";
+cout<<"2.Dost soup                    Rs210"<<"\n";
+
+cout<<"Enter your choice :";
+cin>>m;
+
+if(m==1){
+    n=150;
+    calculate(n);
+}
+else if(m==2){
+    n=210;
+     calculate(n);
+}
+}
 
 int main(){
+    int j;
+    
+  cout<<"--------MENU---------"<<endl;
+  cout<<"1.Snacks"<<"\n"; 
+  cout<<"2.Soup"<<"\n";
 
-   std:: vector<int>v;
-    v.push_back(10);
-    v.push_back(20);
+  cout<<"Select :";
+ cin>>j;
 
-    for(int i=0;i<v.size();i++){
-        std::cout<<v[i]<<"\n";
-    }
+ if(j==1){
+    snacks();
+ } 
+ else if(j==2){
+  soup();
+ }
 
-    std::vector<int>::iterator it;
-    for(it=v.begin();it!=v.end();it++){
-        std::cout<<*it<<std::endl;
-    }
+   
 }
